6516548960cb9bd4e4343d485bff919a14e9557e
Francesco Sacripante <francesco.sacripante@studio.unibo.it>
2025-04-30 17:03:35 +0200

    added timer ( to be refined)

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import socket as sk
 import time
 import threading
-import keyboard as k
 global ack_received
 global num_starting_send 
 global timeout
@@ -15,6 +14,12 @@ def receive(sock):
         num =int(data[0])
         ack[num % WINDOW_LENGHT] = True
         # print(ack)
+def timer():
+    while True:
+        if timeout >= 0.0:
+            time.sleep(1)
+            timeout -= 1
+        
 
 
 # Create il socket UDP
@@ -23,45 +28,30 @@ sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 server_address = ('localhost', 10000)
 num_starting_send = 0
 ack_received = -1
+timeout = 10.0
 print("start")
 receiving_thread = threading.Thread(target = receive,args= (sock,))
-receiving_thread.start()
+timer_thread = threading.Thread(target = timer)
 ack = [True for i in range(WINDOW_LENGHT)]
-timeout = 40.0
+timer_thread.start()
+receiving_thread.start()
 first = True
 while True:
     if first and num_starting_send == 3:
         first = False
         num_starting_send += 1
-    while timeout < 0.0 or not ack[num_starting_send % WINDOW_LENGHT]:
-        print(ack)
-        # pass
-    if timeout >= 0 :
+    while timeout <= 0.0 or not ack[num_starting_send % WINDOW_LENGHT]:
+        message = f"{num_starting_send % WINDOW_LENGHT}packet"
+        print("invio ",num_starting_send)
+        sock.sendto(message.encode(),server_address)
+    
+    if timeout > 0 :
         message = f"{num_starting_send % WINDOW_LENGHT}packet"
         print("invio ",num_starting_send)
         sock.sendto(message.encode(),server_address)
         time.sleep(1)
         ack[num_starting_send % WINDOW_LENGHT] = False
         num_starting_send += 1
-
-
+    else:
+        timeout = 10.0
         
-# try:
-
-#     # inviate il messaggio
-#     print ('sending "%s"' % message)
-#     time.sleep(2) #attende 2 secondi prima di inviare la richiesta
-#     sent = sock.sendto(message.encode(), server_address)
-
-#     # Ricevete la risposta dal server
-#     print('waiting to receive from')
-#     data, server = sock.recvfrom(4096)
-#     #print(server)
-#     time.sleep(2)
-#     print ('received message "%s"' % data.decode('utf8'))
-# except Exception as info:
-#     print(info)
-# finally:
-#     print ('closing socket')
-#     sock.close()
-
